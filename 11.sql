d4f63d7e0bbc521098974a37dd6dbade1e771c1d
kirithiv2000 <kirithiv@navgurukul.org>
2022-11-12 20:10:07 +0530

    11 question

--- 11.sql
+++ 11.sql
@@ -1,2 +1,2 @@
-select * from Customer WHERE city = 'London';
-select * from Salesman WHERE city = 'London';
+select * from Customer join (select * from Salesman WHERE city = 'London') as s on s.salesman_id = Customer.salesman_id  WHERE city = 'London';
+
